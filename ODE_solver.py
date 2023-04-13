bc0815df21d742f0428a8da3074583ed3142dcf3
gonzagrau <ggrau@itba.edu.ar>
2023-04-13 20:53:57 -0300

    added higher_order_ODE() and its test

--- ODE_solver.py
+++ ODE_solver.py
@@ -72,6 +72,35 @@ def single_ruku4(T: np.ndarray, f: Callable, x0 : float) -> np.ndarray:
     return x
 
 
+def higher_order_ODE(T: np.ndarray, f: Callable, X_0: np.ndarray, v: np.ndarray) -> np.ndarray:
+    """
+    @param T: time array of len N, defined as the range a:h:b
+    @param f: function of time and x, f(t, x)
+    @param X_0: initial conditions for x(T[0]), x'(T[0]), ... x**(M-1)(T[0])
+    @param v: vector containing of len M+1 containing the coefficients a_0, a_1, ..., a_M
+
+    Applies the Runge-Kutta 4 method (readily implemented in ruku4() ) to a single M-order ODE of the form:
+    a_M*x**(M) + a_(M-1)*x**(M-1) + ... + a_1*x' + a_0*x = f(t, x)
+    using state variables, by calling x = X[0] and building the system
+    {X[0]' = X[1]
+    {...
+    {X[M-2]' = X[M-1]
+    {X[M-1]' = ( f(t, X[0]) - <v[:-1], X> )/v[-1]
+    where <.,.> indicates the dot product.
+
+    @return: x, a 1-dimensional numpy array of len N with the values of x at every instant T[j]
+    """
+    M = len(X_0)
+    if M+1 != len(v):
+        raise ValueError("The dimensions of the coefficients and the initial conditions do not match")
+    F = FunctionArray()
+    for i in range(M-1):
+        F.append(lambda t, X : X[i+1])
+    F.append(lambda t, X : ( f(t, X[0]) - np.dot(v[:-1], X)) / v[-1] )
+    x = ruku4(T, F, X_0)[:, 0]
+    return x
+
+
 def test_ruku4():
     """
     solves the system
@@ -114,6 +143,26 @@ def test_single_ruku4():
     plt.show()
 
 
+def test_higher_order_ODE():
+    """
+    solves the order 2 ODE
+    x'' − 10*x' + 9*x = 5*t, x(0)=−1, x'(0)=2
+    and plots it compared to the analytical solution
+    """
+    T = np.linspace(0, 0.5, 200)
+    coefficients = np.array([9,-10,1])
+    X_0 = np.array([-1, 2])
+    f = lambda t, x : 5*t
+    x = higher_order_ODE(T, f, X_0, coefficients)
+
+    plt.plot(T, x, 'r-')
+    plt.plot(T, 50/81 + 5*T/9 - 2*np.exp(T) + (31/81)*np.exp(9*T), 'b--')
+
+    plt.show()
+
+
+
 if __name__ == '__main__':
     test_ruku4()
     test_single_ruku4()
+    test_higher_order_ODE()
